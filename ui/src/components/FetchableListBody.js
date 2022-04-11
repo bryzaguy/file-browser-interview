81fad1d77457f25d75d8651561258f15cfafb164
Bryant Syme <prefu@hotmail.com>
2022-04-10 22:52:21 -0700

    add scroll to selected for long lists

--- ui/src/components/FetchableListBody.js
+++ ui/src/components/FetchableListBody.js
@@ -1,16 +1,25 @@
 import { Box, TableBody, TableCell, TableRow } from '@mui/material';
 import InsertDriveFileOutlinedIcon from '@mui/icons-material/InsertDriveFileOutlined';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 export default function FilterableListBody({ items, selected, onSelect, onSubmit }) {
   const removeLastBorder = { '&:last-child td': { border: 0 } };
   const alignContent = { display: 'flex', alignItems: 'flex-end' };
+
+  React.useLayoutEffect(() => {
+    if (selected) {
+      var item = document.querySelector(`[data-id="${selected}"]`);
+      item && item.scrollIntoView({behavior: 'smooth', block: 'nearest'});
+    }
+  }, [selected])
 
   return (
     <TableBody>
       {items.map(name => {
         return (
           <TableRow
+            data-id={name}
             key={name}
             sx={removeLastBorder}
             selected={selected === name}
